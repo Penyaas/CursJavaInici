ad274e7d9f8b1368ac2619d3c86c33d40999d3e4
Toni-Penyas-Amengual <penyasamengual@gmail.com>
2022-01-12 09:42:00 +0100

    Videos inicials de 1 a 20

---
+++ src/com/jetbrains/Main.java
@@ -0,0 +1,17 @@
+package com.jetbrains;
+
+public class Main {
+
+    public static void main(String[] args) {
+       /* double a = 5;
+        double b = 7;
+        double c = b/a;
+        //Operador increment c++;
+        System.out.println(c);
+        */
+        final double apulgadas = 2.54;
+            double cm = 6;
+            double resultat = cm/apulgadas;
+            System.out.println("En " + cm + "cm hay " +resultat+ " pulgadas");
+    }
+}
